02ad75cc73d3b681cad41ded3c51790cad2b4887
timothyshen <timothyshen1@hotmail.com>
2024-02-14 21:42:47 +0000

    completed L17

---
+++ src/L17/Q6.rs
@@ -0,0 +1,24 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    // 实现 mixup，不要修改其它代码！
+    fn mixup <V,W> (self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let p1 = Point { x: 5, y: 10 };
+    let p2 = Point { x: "Hello", y: '中'};
+
+    let p3 = p1.mixup(p2);
+
+    assert_eq!(p3.x, 5);
+    assert_eq!(p3.y, '中');
+}
