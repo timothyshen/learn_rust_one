d76f538897ac79df440a50d2adc6e1191c93b11c
timothyshen <timothyshen1@hotmail.com>
2024-02-14 21:48:08 +0000

    added fix

--- src/L17/Q7.rs
+++ src/L17/Q7.rs
@@ -5,13 +5,13 @@ struct Point<T> {
     y: T,
 }
 
-impl Point {
-    fn distance_from_origin(&self) -> T {
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
         (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
 }
 
 fn main() {
-    let p = Point{x: 5, y: 10};
+    let p = Point{x: 5.0_f32, y: 10.0_f32};
     println!("{}",p.distance_from_origin())
 }
