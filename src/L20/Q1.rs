d8ec28b99198151805bbadb2f206559256a8bcc7
timothyshen <timothyshen1@hotmail.com>
2024-02-20 22:33:51 +0000

    add fix

--- src/L20/Q1.rs
+++ src/L20/Q1.rs
@@ -6,13 +6,20 @@ struct Container(i32, i32);
 //    type A;
 //    type B;
 
-trait Contains<A, B> {
+trait Contains {
+    type A;
+    type B;
+
     fn contains(&self, _: &A, _: &B) -> bool;
     fn first(&self) -> i32;
     fn last(&self) -> i32;
 }
 
-impl Contains<i32, i32> for Container {
+impl Contains for Container {
+
+    type A = i32;
+    type B = i32;
+
     fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
         (&self.0 == number_1) && (&self.1 == number_2)
     }
@@ -23,7 +30,7 @@ impl Contains<i32, i32> for Container {
     fn last(&self) -> i32 { self.1 }
 }
 
-fn difference<A, B, C: Contains<A, B>>(container: &C) -> i32 {
+fn difference<C: Contains>(container: &C) -> i32 {
     container.last() - container.first()
 }
 
--- src/L20/Q2.rs
+++ src/L20/Q2.rs
@@ -8,7 +8,9 @@ struct Point<T> {
 }
 
 // 用三种方法填空: 其中两种使用默认的泛型参数，另外一种不使用
-impl Sub for Point<i32> {
+impl<T: Sub<Output = T>> Sub<Point<T>> for Point<T> {
+//   impl<T: Sub<Output = T>> Sub<Self> for Point<T> {
+//   impl<T: Sub<Output = T>> Sub for Point<T> {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self::Output {
