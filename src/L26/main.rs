4c16f00d7e3a1a1aa8a731ced19113a051a01c93
timothyshen <timothyshen1@hotmail.com>
2024-04-04 22:29:30 +0100

    complete L26

---
+++ src/L26/main.rs
@@ -0,0 +1,41 @@
+/* 使用你的答案填空 */
+
+// Q: package 1# 和 2# 的区别是什么 ?
+
+// A: package 1# 是一个二进制包, 而 package 2# 是一个库包
+
+/* 填空 */
+
+// Q: package `hello-package1` 中的库包名称是?
+// A: hello_package1
+
+
+// # 填空
+// .
+// ├── Cargo.lock
+// ├── Cargo.toml
+// ├── src
+// │   ├── lib.rs
+// │   └── main.rs
+
+
+
+// # 创建一个 a package 包含以下包： 
+// # 1. 三个二进制包: `hello-package`, `main1` and `main2`
+// # 2. 一个库包
+// # 并完成以下目录结构的填空
+// .
+// ├── Cargo.toml
+// ├── Cargo.lock
+// ├── src
+// │   ├── main.rs
+// │   ├── lib.rs
+// │   └── bin
+// │       └── main1.rs
+// │       └── main2.rs
+// ├── tests # 存放集成测试文件的目录
+// │   └── some_integration_tests.rs
+// ├── benches # 存放 benchmark 文件的目录dir for benchmark files
+// │   └── simple_bench.rs
+// └── examples # 存放示例文件的目录
+//     └── simple_example.rs
