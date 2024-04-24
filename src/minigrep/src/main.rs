289f21a84a9e66dbb0e946df16bbf3db5322b5bc
timothyshen <timothyshen1@hotmail.com>
2024-04-24 23:48:18 +0100

    segment code

--- src/minigrep/src/main.rs
+++ src/minigrep/src/main.rs
@@ -5,13 +5,28 @@ fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
-    print!("Searching for {}", query);
-    print!("In file {}", file_path);
+    let config = Config::new(&args);
+    print!("Searching for {}", config.query);
+    print!("In file {}", config.file_path);
 
-    let contents  = fs::read_to_string(file_path)
+    let contents  = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config{
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config {
+            query,
+            file_path
+        }
+    }
 }
