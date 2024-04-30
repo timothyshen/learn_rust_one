8358dcdb4a17624627bd0719bdffeba0a3f6aa3a
timothyshen <timothyshen1@hotmail.com>
2024-04-30 22:44:53 +0100

    working on unzip project

---
+++ projects/rust-decompress/src/main.rs
@@ -0,0 +1,22 @@
+use std::fs;
+use std::io;
+
+fn main() {
+    std::process::exit(real_main());
+}
+
+
+fn real_main() -> i32{
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() < 2 {
+        println("Usage: {} <filename>", args[0]);
+        return 1; 
+    }
+
+    let fname = std::path::Path::new(&*args[1]);
+    let file = fs::File::open(&fname).unwrap();
+
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+    
+}
